e2f92d5a2e89300df7d78d4de42344635624dc64
Santiago Urban <urbansanty@gmail.com>
2022-11-24 18:14:54 -0300

    Debug false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(pt2s4-rayp71lb^azw0tgpz)bko$2a9cz=33m0=8llpugm*rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
